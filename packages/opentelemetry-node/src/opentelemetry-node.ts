0b4dd0985566a4e34fbde436ca42c64c498b093d
JamieDanielson <jamieedanielson@gmail.com>
2022-10-19 18:15:40 -0400

    add OTLPTraceExporter to the SDK

--- packages/opentelemetry-node/src/opentelemetry-node.ts
+++ packages/opentelemetry-node/src/opentelemetry-node.ts
@@ -1,8 +1,10 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 
-// TODO: add OTLPTraceExporter
-// @opentelemetry/exporter-trace-otlp-proto
+const traceExporter = new OTLPTraceExporter();
 
 export function addHoneycomb(): NodeSDK {
-  return new NodeSDK();
+  return new NodeSDK({
+    traceExporter,
+  });
 }
