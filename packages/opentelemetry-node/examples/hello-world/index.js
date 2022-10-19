9ad87747c64900027b5e48155d70c0eccdbe381e
JamieDanielson <jamieedanielson@gmail.com>
2022-10-19 18:02:37 -0400

    add addHoneycomb to example app

--- packages/opentelemetry-node/examples/hello-world/index.js
+++ packages/opentelemetry-node/examples/hello-world/index.js
@@ -1,3 +1,7 @@
-const { sayHello } = require('@honeycombio/opentelemetry-node');
+const { sayHello, addHoneycomb } = require('@honeycombio/opentelemetry-node');
+const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
+
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
 console.log(sayHello());
+addHoneycomb().start();
