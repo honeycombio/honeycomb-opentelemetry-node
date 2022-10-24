a7e56ec26d15c043414eb9b0f79f0616fdf103ed
JamieDanielson <jamieedanielson@gmail.com>
2022-10-24 18:38:43 -0400

    working example with extra stuff, cleanup later

--- examples/hello-node/index.js
+++ examples/hello-node/index.js
@@ -1,19 +1,42 @@
 const {
-  Honeycomb,
-  honeycombTraceExporter,
+  HoneycombHttp,
+  honeycombHttpTraceExporter,
 } = require('@honeycombio/opentelemetry-node');
 const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
 
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
-const sayHello = () => 'Hello world!';
-
-console.log(sayHello());
-const sdk = Honeycomb({
-  traceExporter: honeycombTraceExporter,
+const sdk = HoneycombHttp({
+  traceExporter: honeycombHttpTraceExporter({
+    apiKey: 'test-key',
+  }),
 });
 
 sdk
   .start()
   .then(() => console.log('Tracing initialized'))
   .catch((error) => console.log('Error initializing tracing', error));
+
+const http = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const otel = require('@opentelemetry/api');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'http/plain');
+  const sayHello = () => 'Hello world!';
+  const tracer = otel.trace.getTracer('hello-world-tracer');
+  tracer.startActiveSpan('main', (span) => {
+    console.log('saying hello to the world');
+    span.setAttribute('important_note', 'heygirl');
+    span.end();
+  });
+  sayHello();
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
