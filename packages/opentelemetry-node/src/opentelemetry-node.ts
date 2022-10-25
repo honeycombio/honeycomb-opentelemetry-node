d01f27d2e7db6ec60fe5b6c22e8fd9c9cb160779
JamieDanielson <jamieedanielson@gmail.com>
2022-10-25 09:01:19 -0400

    http, not grpc

--- packages/opentelemetry-node/src/opentelemetry-node.ts
+++ packages/opentelemetry-node/src/opentelemetry-node.ts
@@ -1,5 +1,5 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import { honeycombTraceExporter } from './grpc-trace-exporter';
+import { honeycombTraceExporter } from './http-trace-exporter';
 
 export function Honeycomb(): NodeSDK {
   return new NodeSDK({
