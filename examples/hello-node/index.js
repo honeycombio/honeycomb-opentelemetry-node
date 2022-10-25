4b8d6024ab805decd06782b4edde9b5e57f064ab
JamieDanielson <jamieedanielson@gmail.com>
2022-10-25 09:23:39 -0400

    use trace exporter and sdk in example

--- examples/hello-node/index.js
+++ examples/hello-node/index.js
@@ -2,15 +2,39 @@ const {
   Honeycomb,
   honeycombTraceExporter,
 } = require('@honeycombio/opentelemetry-node');
-const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
+const {
+  diag,
+  DiagConsoleLogger,
+  DiagLogLevel,
+  trace,
+} = require('@opentelemetry/api');
+
+const http = require('node:http');
+const hostname = '127.0.0.1';
+const port = 3000;
 
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
-const sayHello = () => 'Hello world!';
-
-console.log(sayHello());
 const sdk = Honeycomb({
   traceExporter: honeycombTraceExporter,
+});
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'http/plain');
+  const sayHello = () => 'Hello world!';
+  const tracer = trace.getTracer('hello-world-tracer');
+  tracer.startActiveSpan('main', (span) => {
+    console.log('saying hello to the world');
+    span.setAttribute('message', 'hello-world');
+    span.end();
+  });
+  sayHello();
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 sdk
