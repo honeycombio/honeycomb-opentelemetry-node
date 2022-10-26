08fb5f57523ba58df33b809eb73527a89ed0535c
Jamie Danielson <jamieedanielson@gmail.com>
2022-10-26 12:26:43 -0400

    feat: add http/protobuf trace exporter (#61)
    ## Which problem is this PR solving?
    
    - Closes #11 
    
    ## Short description of the changes
    
    - add trace exporter for http/protobuf
    - add tests
    - use in example app
    
    This doesn't yet update the interface for working with the Honeycomb SDK
    directly. Rather, it provides the http/protobuf trace exporter to be
    used with the Node SDK. Issue #40 should create a way to interface with
    the SDK.
    
    ## How to verify that this has the expected result
    
    ```shell
    export HONEYCOMB_API_KEY=<yourapikey>
    npm run clean-all && npm run build-all && npm run example-node
    curl localhost:3000
    ```
    
    Co-authored-by: Emily Ashley <emilyashley@users.noreply.github.com>

--- packages/opentelemetry-node/src/index.ts
+++ packages/opentelemetry-node/src/index.ts
@@ -1,1 +1,2 @@
 export { Honeycomb } from './opentelemetry-node';
+export { honeycombTraceExporter } from './http-trace-exporter';
--- packages/opentelemetry-node/test/honeycomb-options.test.ts
+++ packages/opentelemetry-node/test/honeycomb-options.test.ts
@@ -1,6 +1,10 @@
-import { setDefaultOptions } from '../src/honeycomb-options';
+import { HoneycombOptions } from '../src/honeycomb-options';
 
-test('it should set default honeycomb endpoint', () => {
-  const options = setDefaultOptions();
-  expect(options.endpoint).toEqual('https://api.honeycomb.io');
+test('it should have an apiKey property on the HoneycombOptions object', () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function hasApiKey(object: any): object is HoneycombOptions {
+    return 'apiKey' in object;
+  }
+  const testApiKey = hasApiKey({ apiKey: 'testkey' });
+  expect(testApiKey).toEqual(true);
 });
