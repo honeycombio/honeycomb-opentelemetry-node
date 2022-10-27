954c85a96b63640f1361d86511b4304646e50d29
Mike Goldsmth <goldsmith.mike@gmail.com>
2022-10-27 17:09:58 +0100

    update trace exporter to not take headers

--- src/http-trace-exporter.ts
+++ src/http-trace-exporter.ts
@@ -1,23 +1,27 @@
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { HoneycombOptions } from './honeycomb-options';
+import { isLegacy } from './honeycomb-options';
 
 const HONEYCOMB_API_ENDPOINT = 'https://api.honeycomb.io/v1/traces';
 const TEAM_HEADER_KEY = 'x-honeycomb-team';
+const DATASET_HEADER_KEY = 'x-honeycomb-team';
 const OTLP_HEADER_KEY = 'x-otlp-version';
 const OTLP_PROTO_VERSION = '0.16.0';
 
 export function honeycombTraceExporter({
   apiKey,
-  headers,
+  dataset
 }: HoneycombOptions): OTLPTraceExporter {
   const HoneycombTraceExporterObject = {
     url: HONEYCOMB_API_ENDPOINT,
     headers: {
-      ...headers,
       [OTLP_HEADER_KEY]: OTLP_PROTO_VERSION,
       [TEAM_HEADER_KEY]: apiKey,
     },
   };
+  if (isLegacy({ apiKey }) && dataset) {
+    HoneycombTraceExporterObject.headers[DATASET_HEADER_KEY] = dataset;
+  }
 
   return new OTLPTraceExporter(HoneycombTraceExporterObject);
 }
