cc46d7b010ebd7cf3a3cf6f97143334b811805b4
Mike Goldsmith <goldsmith.mike@gmail.com>
2022-10-27 16:39:46 +0100

    Configure Honeycomb resource (#60)
    ## Which problem is this PR solving?
    Configures the Honeycomb Resource with attributes from the Honeycomb
    distro spec.
    - Closes #12 
    
    ## Short description of the changes
    - Updates the `addResource` to add required attributes
    - Update and add unit tests to verify behaviour
    
    ## How to verify that this has the expected result
    The resource returned by `addResource` returns a Resource object with
    the Honeycomb specific attributes set.

--- src/resource-builder.ts
+++ src/resource-builder.ts
@@ -1,13 +1,27 @@
 import { Resource, ResourceAttributes } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { getEnv } from '@opentelemetry/core';
+import { HoneycombOptions } from './honeycomb-options';
 
-// TODO: detect env vars for resources
-// maybe use getEnv() from @opentelemetry/core
-// https://github.com/open-telemetry/opentelemetry-js/blob/a7d053ae5a9fb073ccc3b639c3359fba19594e3d/packages/opentelemetry-core/src/platform/node/environment.ts
+// TODO: generate as part of the build process from package.json
+export const version = '0.1.0';
 
-// TODO: add honeycomb attributes for distro version and runtime_version
-// https://github.com/open-telemetry/opentelemetry-js/blob/a7d053ae5a9fb073ccc3b639c3359fba19594e3d/packages/opentelemetry-core/src/platform/node/sdk-info.ts
+export function addResource(options?: HoneycombOptions): Resource {
+  // get OTel environemt which includes common properties like service name
+  const env = getEnv();
 
-export function addResource(): Resource {
-  const resourceAttrs: ResourceAttributes = {};
+  const resourceAttrs: ResourceAttributes = {
+    'honeycomb.distro.version': version,
+    'honeycomb.distro.runtime_version': process.versions.node,
+  };
+
+  // set service name attribute, prefer env var over options
+  const serviceName: string | undefined =
+    env?.OTEL_SERVICE_NAME || options?.serviceName;
+  if (serviceName) {
+    resourceAttrs[SemanticResourceAttributes.SERVICE_NAME] = serviceName;
+  }
+
+  // combine default resource with honeycomb resource
   return new Resource(resourceAttrs);
 }
--- test/resource-builder.test.ts
+++ test/resource-builder.test.ts
@@ -1,7 +1,51 @@
-import { addResource } from '../src/resource-builder';
+import { addResource, version } from '../src/resource-builder';
 import { Resource } from '@opentelemetry/resources';
+import { HoneycombOptions } from '../src/honeycomb-options';
 
 test('it should return a Resource', () => {
   const resource = addResource();
   expect(resource instanceof Resource);
+  expect(resource.attributes['honeycomb.distro.version']).toEqual(version);
+  expect(resource.attributes['honeycomb.distro.runtime_version']).toEqual(
+    process.versions.node,
+  );
+  expect(resource.attributes['service.name']).toEqual(undefined);
+});
+
+test('it should use options service name when set', () => {
+  const options: HoneycombOptions = {
+    apiKey: 'dummy-key',
+    serviceName: 'my-service',
+  };
+  const resource = addResource(options);
+  expect(resource instanceof Resource);
+  expect(resource.attributes['honeycomb.distro.version']).toEqual(version);
+  expect(resource.attributes['honeycomb.distro.runtime_version']).toEqual(
+    process.versions.node,
+  );
+  expect(resource.attributes['service.name']).toEqual('my-service');
+});
+
+describe('when OTEL_SERVICE_NAME env var is set', () => {
+  beforeEach(() => {
+    process.env.OTEL_SERVICE_NAME = 'my-awesome-service';
+  });
+
+  afterEach(() => {
+    delete process.env.OTEL_SERVICE_NAME;
+  });
+
+  test('it should prefer service name in env var over options', () => {
+    const options: HoneycombOptions = {
+      apiKey: 'dummy-key',
+      serviceName: 'my-service',
+    };
+    const resource = addResource(options);
+    expect(resource instanceof Resource);
+    expect(resource.attributes['honeycomb.distro.version']).toEqual(version);
+    expect(resource.attributes['honeycomb.distro.runtime_version']).toEqual(
+      process.versions.node,
+    );
+    expect(resource.attributes['service.name']).toEqual('my-awesome-service');
+  });
 });
