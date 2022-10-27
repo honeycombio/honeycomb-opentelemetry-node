087802b48bc5f3d7899386f69cd1775960a6f110
Mike Goldsmth <goldsmith.mike@gmail.com>
2022-10-27 17:10:14 +0100

    remove traces exporter headers test

--- test/http-trace-exporter.test.ts
+++ test/http-trace-exporter.test.ts
@@ -15,16 +15,3 @@ test('it should set properties on the exporter object', () => {
   // TODO: get value instead of hard-coding
   expect(traceExporter.headers['x-otlp-version']).toBe('0.16.0');
 });
-
-test('it should prefer the apiKey and x-otlp-version property from HoneycombOptions over passed-in headers', () => {
-  // TODO: if a duplicate key is found, which should win?
-  // i.e. if x-honeycomb-team is passed as a header
-  // AND passed as an API Key... which should win?
-  const traceExporter = honeycombTraceExporter({
-    apiKey: 'foo',
-    headers: { 'x-honeycomb-team': 'bar', 'x-otlp-version': '42' },
-  });
-  expect(traceExporter.headers['x-honeycomb-team']).toBe('foo');
-  // TODO: get value instead of hard-coding
-  expect(traceExporter.headers['x-otlp-version']).toBe('0.16.0');
-});
