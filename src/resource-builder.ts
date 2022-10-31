176520730a01b777115f695b3c7806f2b3464141
Mike Goldsmth <goldsmith.mike@gmail.com>
2022-10-31 13:42:02 +0000

    make version in package.json into a usable string

--- src/resource-builder.ts
+++ src/resource-builder.ts
@@ -2,16 +2,14 @@ import { Resource, ResourceAttributes } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { getEnv } from '@opentelemetry/core';
 import { HoneycombOptions } from './honeycomb-options';
-
-// TODO: generate as part of the build process from package.json
-export const version = '0.1.0';
+import { VERSION } from './version';
 
 export function addResource(options?: HoneycombOptions): Resource {
   // get OTel environemt which includes common properties like service name
   const env = getEnv();
 
   const resourceAttrs: ResourceAttributes = {
-    'honeycomb.distro.version': version,
+    'honeycomb.distro.version': VERSION,
     'honeycomb.distro.runtime_version': process.versions.node,
   };
 
