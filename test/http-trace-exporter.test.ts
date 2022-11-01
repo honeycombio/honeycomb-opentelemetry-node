668ec1f9fa1e546509f895f0da289ea0f935e3ad
Mike Goldsmth <goldsmith.mike@gmail.com>
2022-11-01 12:25:47 +0000

    update traces tests

--- test/http-trace-exporter.test.ts
+++ test/http-trace-exporter.test.ts
@@ -1,17 +1,90 @@
-import { honeycombTraceExporter } from '../src/http-trace-exporter';
+import {
+  honeycombTraceExporter,
+  TEAM_HEADER_KEY,
+  DATASET_HEADER_KEY,
+  OTLP_HEADER_KEY,
+  OTLP_PROTO_VERSION,
+} from '../src/http-trace-exporter';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { DEFAULT_API_ENDPOINT } from '../src/honeycomb-options';
+
+const dataset = 'my-dataset';
+const apikey = '0000000000000000000000'; // 22 chars
+const classicApikey = '00000000000000000000000000000000'; // 32 chars
 
 test('it should return an OTLPTraceExporter', () => {
-  const traceExporter = honeycombTraceExporter({ apiKey: 'foo' });
+  const traceExporter = honeycombTraceExporter();
   expect(traceExporter instanceof OTLPTraceExporter);
 });
 
-test('it should set properties on the exporter object', () => {
-  const traceExporter = honeycombTraceExporter({
-    apiKey: 'foo',
+describe('with a regular apikey', () => {
+  test('it should set the team and not the dataset headers', () => {
+    const traceExporter = honeycombTraceExporter({
+      apiKey: apikey,
+      dataset: dataset,
+    });
+    expect(traceExporter.url).toBe(DEFAULT_API_ENDPOINT);
+    expect(traceExporter.headers[OTLP_HEADER_KEY]).toBe(OTLP_PROTO_VERSION);
+    expect(traceExporter.headers[TEAM_HEADER_KEY]).toBe(apikey);
+    expect(traceExporter.headers[DATASET_HEADER_KEY]).toBeUndefined();
   });
-  expect(traceExporter.url).toBe('https://api.honeycomb.io/v1/traces');
-  expect(traceExporter.headers['x-honeycomb-team']).toBe('foo');
-  // TODO: get value instead of hard-coding
-  expect(traceExporter.headers['x-otlp-version']).toBe('0.16.0');
+
+  describe('when env vars are set', () => {
+    beforeEach(() => {
+      process.env.HONEYCOMB_API_KEY = apikey;
+      process.env.HONEYCOMB_DATASET = dataset;
+    });
+
+    afterEach(() => {
+      delete process.env.HONEYCOMB_API_KEY;
+      delete process.env.HONEYCOMB_DATASET;
+    });
+
+    test('it should set the team and dataset headers from env vars', () => {
+      const traceExporter = honeycombTraceExporter({
+        apiKey: 'apikey',
+        dataset: 'dataset',
+      });
+      expect(traceExporter.url).toBe(DEFAULT_API_ENDPOINT);
+      expect(traceExporter.headers[OTLP_HEADER_KEY]).toBe(OTLP_PROTO_VERSION);
+      expect(traceExporter.headers[TEAM_HEADER_KEY]).toBe(apikey);
+      expect(traceExporter.headers[DATASET_HEADER_KEY]).toBeUndefined();
+    });
+  });
+});
+
+describe('with a classic apikey', () => {
+  test('it should set the team and dataset headers', () => {
+    const traceExporter = honeycombTraceExporter({
+      apiKey: classicApikey,
+      dataset: dataset,
+    });
+    expect(traceExporter.url).toBe(DEFAULT_API_ENDPOINT);
+    expect(traceExporter.headers[OTLP_HEADER_KEY]).toBe(OTLP_PROTO_VERSION);
+    expect(traceExporter.headers[TEAM_HEADER_KEY]).toBe(classicApikey);
+    expect(traceExporter.headers[DATASET_HEADER_KEY]).toBe(dataset);
+  });
+
+  describe('when env vars are set', () => {
+    beforeEach(() => {
+      process.env.HONEYCOMB_API_KEY = classicApikey;
+      process.env.HONEYCOMB_DATASET = dataset;
+    });
+
+    afterEach(() => {
+      delete process.env.HONEYCOMB_API_KEY;
+      delete process.env.HONEYCOMB_DATASET;
+    });
+
+    test('it should set the team and dataset headers from env vars', () => {
+      const traceExporter = honeycombTraceExporter({
+        apiKey: 'apikey',
+        dataset: 'dataset',
+      });
+      expect(traceExporter.url).toBe(DEFAULT_API_ENDPOINT);
+      expect(traceExporter.headers[OTLP_HEADER_KEY]).toBe(OTLP_PROTO_VERSION);
+      expect(traceExporter.headers[TEAM_HEADER_KEY]).toBe(classicApikey);
+      expect(traceExporter.headers[DATASET_HEADER_KEY]).toBe(dataset);
+    });
+  });
 });
