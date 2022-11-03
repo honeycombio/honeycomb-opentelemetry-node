52681ec2a878bb725f6f37f73a9a4cc03c20ec75
Mike Goldsmth <goldsmith.mike@gmail.com>
2022-11-03 16:18:02 +0000

    add grpc trace exporter

---
+++ src/grpc-trace-exporter.ts
@@ -0,0 +1,35 @@
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
+import {
+  DATASET_HEADER_KEY,
+  OTLP_HEADER_KEY,
+  OTLP_PROTO_VERSION,
+  TEAM_HEADER_KEY,
+} from './exporter-consts';
+import {
+  HoneycombOptions,
+  isClassic,
+  computeOptions,
+} from './honeycomb-options';
+import { Metadata } from '@grpc/grpc-js';
+jest.useFakeTimers();
+
+/**
+ * Builds and returns an OTLP Traces exporter that sends telemetry over gRPC
+ * @param options The HoneycombOptions used to configure the exporter
+ * @returns the configured OTLPTracesExporter instance
+ */
+export function configureHoneycombGrpcTraceExporter(
+  options?: HoneycombOptions,
+): OTLPTraceExporter {
+  const opts = computeOptions(options);
+  const metadata = new Metadata();
+  metadata.set(OTLP_HEADER_KEY, OTLP_PROTO_VERSION);
+  metadata.set(TEAM_HEADER_KEY, opts.tracesApiKey || '');
+  if (isClassic(opts?.tracesApiKey)) {
+    metadata.set(DATASET_HEADER_KEY, opts.dataset || '');
+  }
+  return new OTLPTraceExporter({
+    url: opts.tracesEndpoint,
+    metadata,
+  });
+}
