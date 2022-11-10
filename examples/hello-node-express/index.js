596ed42604a074913b5ccaf9526adea141cc1377
JamieDanielson <jamieedanielson@gmail.com>
2022-11-10 14:45:16 -0500

    add example app using express

---
+++ examples/hello-node-express/index.js
@@ -0,0 +1,39 @@
+const { configureHoneycombSDK } = require('@honeycombio/opentelemetry-node');
+const { trace } = require('@opentelemetry/api');
+
+const express = require('express');
+const app = express();
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const sdk = configureHoneycombSDK({
+  apiKey: process.env.HONEYCOMB_API_KEY || '',
+  serviceName: process.env.OTEL_SERVICE_NAME || 'hello-node-express',
+  debug: true,
+});
+
+// alternatively, use HONEYCOMB_API_KEY and OTEL_SERVICE_NAME and DEBUG environment variables
+// const sdk = configureHoneycombSDK();
+
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  const sayHello = () => 'Hello world!';
+  const tracer = trace.getTracer('hello-world-tracer');
+  tracer.startActiveSpan('main', (span) => {
+    console.log('saying hello to the world');
+    span.setAttribute('message', 'hello-world');
+    span.end();
+  });
+  sayHello();
+  res.end('Hello, World!\n');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Listening on http://${hostname}:${port}/`);
+});
+
+sdk
+  .start()
+  .then(() => console.log('Tracing initialized'))
+  .catch((error) => console.log('Error initializing tracing', error));
