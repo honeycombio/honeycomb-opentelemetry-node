e73fdcfffe0d2f110a856469273f6d978f873e5c
Phillip Carter <pcarter@fastmail.com>
2022-11-19 02:06:39 +0000

    schloopers

--- examples/hello-node-express-ts/index.ts
+++ examples/hello-node-express-ts/index.ts
@@ -8,36 +8,41 @@ const port = 3000;
 
 const tracer = trace.getTracer('hello-world-tracer');
 
-app.get('/', asyncHandler(async (_req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  const sayHello = () => 'Hello world!';
-  // new context based on current, with key/values added to baggage
-  const ctx = propagation.setBaggage(
-    context.active(),
-    propagation.createBaggage({
-      for_the_children: { value: 'another important value' },
-    }),
-  );
-  // within the new context, do some "work"
-  await context.with(ctx, async () => {
-    await tracer.startActiveSpan('sleep', async (span) => {
-      console.log('saying hello to the world');
-      span.setAttribute('message', 'hello-world');
-      span.setAttribute('delay_ms', 100);
-      console.log('sleeping a bit!')
-      await sleepy();
-      span.end();
+app.get(
+  '/',
+  asyncHandler(async (_req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    const sayHello = () => 'Hello world!';
+    // new context based on current, with key/values added to baggage
+    const ctx = propagation.setBaggage(
+      context.active(),
+      propagation.createBaggage({
+        for_the_children: { value: 'another important value' },
+      }),
+    );
+    // within the new context, do some "work"
+    await context.with(ctx, async () => {
+      await tracer.startActiveSpan('sleep', async (span) => {
+        console.log('saying hello to the world');
+        span.setAttribute('message', 'hello-world');
+        span.setAttribute('delay_ms', 100);
+        console.log('sleeping a bit!');
+        await sleepy();
+        span.end();
+      });
     });
-  });
-  sayHello();
-  res.end('Hello, World!\n');
-}));
+    sayHello();
+    res.end('Hello, World!\n');
+  }),
+);
 
 function sleepy() {
-  return new Promise(() => setTimeout(() => {
-    console.log('awake now!');
-  }, 100));
+  return new Promise(() =>
+    setTimeout(() => {
+      console.log('awake now!');
+    }, 100),
+  );
 }
 
 app.listen(port, hostname, () => {
